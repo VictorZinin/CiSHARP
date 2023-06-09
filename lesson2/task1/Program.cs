85351edc2c2b47aad7a24b20909bd40afcf59070
Victor <zvic21@gmail.com>
2023-06-09 16:12:54 +0300

    lesson2

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,99 @@
+﻿// программа получает на вход 4 числа и находит наименьшее и наибольшее из них
+       
+       
+       
+        Console.Clear();
+        System.Console.WriteLine("Enter 4 numbers");
+        string line = Console.ReadLine();
+        string[] splitString = line.Split(' ');
+
+        int num1 = Convert.ToInt32(splitString[0]);
+        int num2 = Convert.ToInt32(splitString[1]);
+        int num3 = Convert.ToInt32(splitString[2]);
+        int num4 = Convert.ToInt32(splitString[3]);
+
+        int maxValue = num1;
+        int minValue = num2;
+        
+        if (num2 > maxValue )
+        {
+            maxValue = num2;
+            minValue = num1;
+            if (num3 > maxValue)
+            {
+                maxValue = num3;            
+                if (num4 > maxValue)
+                {
+                    maxValue = num4;
+                    
+                }
+                else if (num4 < minValue)
+                {
+                    minValue = num4;
+                }
+            }
+            else if (num4 > maxValue)
+            {
+                    maxValue = num4;
+                    if (num3 < minValue)
+                    {
+                        minValue = num3;
+                        if (num4 < minValue)
+                        {
+                            minValue = num4;
+                        }
+                    }
+            }
+            else 
+            {
+                if (num3 < minValue)
+                    {
+                        minValue = num3;
+                        if (num4 < minValue)
+                        {
+                            minValue = num4;
+                        }
+                    }
+                if (num4 < minValue) 
+                {
+                    minValue = num4;
+                }   
+            }   
+        } 
+        else if (num3 > maxValue)
+            {
+                maxValue = num3;            
+                if (num4 > maxValue)
+                {
+                    maxValue = num4;
+                }
+                else if (num4 < minValue)
+                {
+                    minValue = num4;
+                }
+                
+            }
+            
+        else if (num4 > maxValue)
+        {
+            maxValue = num4;
+            
+                if (num3 < minValue)
+                {
+                    minValue = num3;
+                }
+        }
+        else
+        {
+            if (num3 < minValue)
+            {
+                minValue = num3;
+            }
+            if (num4 < minValue) 
+            {
+                minValue = num4;
+            }
+        }
+        Console.WriteLine($"Maximum number - {maxValue}");
+        Console.WriteLine($"Minimum number - {minValue}");
+
---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// В банке в зависимости от суммы вклада начисляемый процент по вкладу может отличаться.
+// Напишите программу, в которую пользователь вводит сумму вклада.
+// Если сумма вклада меньше 100, то начисляется 5%. Если сумма вклада от 100 до 200 включительно,  то начисляется 7%.
+// Если сумма вклада больше 200, то начисляется 10%. В конце программа должна выводить сумму вклада с начисленными процентами.
+// Не вложив деньги, вклад открыть нельзя, поэтому ситуации с отрицательными числами или 0 не рассматриваем.
+
+
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.Clear();
+System.Console.Write("Enter amount you want to deposit into your bank account : ");   
+double bankDeposit = Convert.ToDouble(Console.ReadLine());
+double sum = 0.0;
+if (bankDeposit < 100)
+{
+    sum = bankDeposit * 1.05;
+}
+else if ( bankDeposit >= 100 && bankDeposit <= 200)
+{
+   sum = bankDeposit * 1.07;
+}
+else
+{
+    sum = bankDeposit * 1.1;
+}
+System.Console.WriteLine($"Amount after interest: {sum}");
+
+    }
+}
